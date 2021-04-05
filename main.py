9984b7157be667fef310b15ba6db04d540fd666c
tamagram <tamasupaka@gmail.com>
2021-04-06 02:19:03 +0900

    :tada: First commit.
    Hello world.

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+def main():
+    app.debug = True
+    app.run()
+
+if __name__ == "__main__":
+    main()
