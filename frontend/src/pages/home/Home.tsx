b7afd51ee918d97267e0e4a0382420eb62ebd7f4
tamagram <tamagram64@gmail.com>
2021-06-17 23:07:59 +0900

    :fire: Remove code that fails to style.

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,26 +1,17 @@
 import React from 'react'
 import styled from 'styled-components'
-import StyledHeader from './Header'
-import StyledNavbar from './Navbar'
-import StyledSide from './Side'
-import StyledTweet from './Tweet'
 
 const StyledHome: React.FC = () => {
   return (
     <Home>
-      <StyledHeader />
-      <StyledNavbar />
-      <StyledTweet />
-      <StyledSide />
+      home
     </Home>
   )
 }
 
 const Home = styled.div`
-  /* Home */
   position: relative;
 
   background: #FFFFFF;
 `
-
 export default StyledHome
